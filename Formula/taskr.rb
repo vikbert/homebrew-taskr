ed74168321508020f198207953fe59882eba39a6
Xun Zhou <xun.zhou@mail.schwarz>
2026-01-06 14:59:11 +0100

    updates

--- Formula/taskr.rb
+++ Formula/taskr.rb
@@ -4,8 +4,8 @@
 class Taskr < Formula
   desc "Task runner / simpler Make alternative written in Go (forked version)"
   homepage "https://github.com/vikbert/taskr"
-  url "https://github.com/vikbert/taskr/archive/refs/tags/v3.47.6.tar.gz"
-  sha256 "e584bc62f14e2af82be659e2e18ef338e61aa96beb6c7a06e9cb34cf4987d301"
+  url "https://github.com/vikbert/taskr/archive/refs/tags/v3.47.7.tar.gz"
+  sha256 "20705d89f0aae70b7927661e44c9130bc8d66e59366c7caea92174bf68dd0d31"
   license "MIT"
   head "https://github.com/vikbert/taskr.git", branch: "main"
 
